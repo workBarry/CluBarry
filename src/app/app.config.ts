5cbfca1879a8d5137b59eae5862363f9fe55e524
barry.chen <barry.chen@pollex.com.tw>
2026-07-10 09:37:36 +0800

    fix: add trackBy to my-clubs ngFor, fix create-club API call format

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +7,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
     provideRouter(routes),
   ],
 };
